487da36da4f2919bd630b725cfb4bdfa2303be31
hhack <hans.f.hack@googlemail.com>
2024-02-14 10:00:51 +0100

    use all adjustmens from lod-vocab repo

--- bin/compute_cbds.py
+++ bin/compute_cbds.py
@@ -2,7 +2,6 @@ import argparse
 import json
 from pathlib import Path
 from rdflib import Graph, BNode
-from rdflib.namespace import split_uri
 
 parser = argparse.ArgumentParser(
     description="Compute the concise bounded description for each subject in the input file.")
@@ -25,22 +24,15 @@ all.parse(args.source)
 cbds = {}
 
 for subject in all.subjects():
-    try:
-        namespace = str(subject)
-        if subject == args.base:
-            namespace = split_uri(subject)[0]
-        if args.base in namespace:
-            cbd = all.cbd(subject)
-            name = subject.removeprefix(args.base)
-            name = name.replace("/", "_")
-            name = f"_{name}"
-            cbd_dict = json.loads(cbd.serialize(format="json-ld"))
-            out_path = args.output / name
-            outpath = f"{out_path}.json"
-            with open(outpath, 'w') as cbd_file:
-                json.dump(cbd_dict, cbd_file, indent=2)
-    except ValueError:
-        next
+    if not isinstance(subject, BNode):
+        cbd = all.cbd(subject)
+        name = subject.removeprefix(args.base)
+        name = name.replace("/", "_")
+        cbd_dict = json.loads(cbd.serialize(format="json-ld"))
+        out_path = args.output / name
+        outpath = f"{out_path}.json"
+        with open(outpath, 'w') as cbd_file:
+            json.dump(cbd_dict, cbd_file, indent=2)
 
 
 
